fd36d71d63e53e01169b2791798ff4800bf0e002
includemohammad <127312425+includemohammad@users.noreply.github.com>
2025-04-23 15:57:14 +0330

    Add files via upload

---
+++ hop1.m
@@ -0,0 +1,32 @@
+clc;
+clear;
+close all;
+
+%% Create and Train Hopfield Network
+
+P=[ 1 -1
+   -1  1];
+
+net=newhop(P);
+
+% view(net);
+
+%% Test Network
+
+a=[0.8 0.2]';
+
+y=net({1 10},[],{a});
+
+y=[a cell2mat(y)];
+
+figure;
+plot(y(1,:),y(2,:),'b');
+hold on;
+plot(a(1,:),a(2,:),'bx');
+plot(P(1,:),P(2,:),'rs','LineWidth',2);
+legend('Trajectory','Start Point','Fixed Point');
+legend('Location','NorthEastOutside');
+grid on;
+
+
+
